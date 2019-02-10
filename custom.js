1e021f5039ff1ed112e15cdb7285b86ba0dfb8b9
mwaiyusuf <mwaigalo5@gmail.com>
2019-02-10 14:57:40 +0300

    button

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -8,9 +8,7 @@ function scrollFunction() {
     document.getElementById("myBtn").style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
 }
